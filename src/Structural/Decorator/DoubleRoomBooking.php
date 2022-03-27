d401ea4074fd6544f6d81cac6e5a35d5a45f0f7b
Gleb Kalinin <sogener@yandex.ru>
2022-03-27 17:06:38 +0500

    Implement decorator

---
+++ src/Structural/Decorator/DoubleRoomBooking.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Structural\Decorator;
+
+class DoubleRoomBooking implements Booking
+{
+
+    public function calculatePrice(): int
+    {
+        return 40;
+    }
+
+    public function getDescription(): string
+    {
+        return 'double room';
+    }
+}
